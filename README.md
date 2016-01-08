7e00c2712eaa166d20a7533b774cffb64bde68b6
Stephane Labrosse <stephane@makatea.lst.ens-lyon.fr>
2016-01-08 17:10:10 +0100

    added support for general boundary conditions

--- README.md
+++ README.md
@@ -9,17 +9,23 @@ The curve of critical Rayleigh number as function of wavenumber is computed
 and plotted and the minimum value of the Rayleigh number is obtained along
 with the corresponding value of the wavenumber.
 
-Two choices of boundary conditions are possible: free-slip or no-slip, applying
-to both boundaries. Cases with mixed BCs (Robin) or with different BCs at the
-top and bottom still need to be implemented.
+Any type of the classical boundary conditions can be computed at
+either boundary for each variable, horizontal velocity (u), vertical
+velocity (w), and temperature (t). Example calculations are provided
+and cna be turned on or off by setting the options at the beginning of
+the file:
+* COMPUTE_FREESLIP for free slip BCs at both boundaries
+* COMPUTE_NOSLIP for rigid BCs at both boundaries
+* COMPUTE_FREERIGID for free slip top BC and rigid bottom
+Resulting figures are also provided. Results for other BCs can be
+obtained following this last example.
 
-Options can be changed at the beginning of the file:
- - COMPUTE_FREESLIP and COMPUTE_NOSLIP are self-explaining boolean variables
- - NCHEB is the number of Chebyshev points to use in the computation
- - FTSZ is the fontsize for the annotation on the plots
+Other options can be changed at the beginning of the file:
+* NCHEB is the number of Chebyshev points to use in the computation
+* FTSZ is the fontsize for the annotation on the plots
 
 The calculation uses an implementation of DMSuite in Python available on github
-as part of the pyddx package (https://github.com/labrosse/pyddx).
+as part of the pyddx package (https://github.com/labrosse/dmsuite).
 DMSuite was originally developed for matlab by
 Weidemann and Reddy and explained in ACM Transactions of Mathematical
 Software, 4, 465-519 (2000). The present code is based on an octave code
--- crit_ra.py
+++ crit_ra.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Finds critical Rayleigh number.
 
@@ -9,18 +9,157 @@ Can do both the no-slip and free-slip BCs, applying to both boundaries.
 import math
 import numpy as np
 from scipy import linalg
-import pyddx.sc.dmsuite as dm
+import dmsuite.dmsuite as dm
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sprintf
 
 ######## Options #######
-COMPUTE_FREESLIP = True
-COMPUTE_NOSLIP = True
+# Free slip at both boudaries
+COMPUTE_FREESLIP = False
+# No slip at both boundaries
+COMPUTE_NOSLIP = False
+COMPUTE_FREERIGID = True
 NCHEB = 10
 FTSZ = 14
-
 ######################
-def eigval_freeslip(wnk, ranum, ncheb):
+
+def eigval_general(wnk, ranum, ncheb, **kwargs):
+    """
+    Eigenvalue for given wavenumber and Rayleigh number with general BCs
+
+    The boundary conditions are
+    a_1 f(1) + b_1 f'(1)  = c_1
+    a_N f(-1) + b_N f'(-1) = c_N
+    for each of the following variables: u (horizontal velocity),
+    w (vertical velocity), t (temperature)
+    The values are passed as part of the **kwargs, with keywords
+    bcsX for variable X.
+    Default values are the Dirichlet conditions for all variables at both
+    boundaries, ie: bcsX = np.array([[1, 0, 0], [1, 0, 0]], float)
+
+    INPUT
+    wnk = horizontal wavenumber of the perturbation
+    ranum = Rayleigh number
+    ncheb = number of Chebyshev points in vertical direction
+
+    OUTPUT
+    eigv = eigenvalue with the largest real part (fastest growing if positive).
+    """
+    # Default boundary conditions. 
+    bcsu = np.array([[1, 0, 0],[1, 0, 0]], float)
+    bcsw = np.array([[1, 0, 0],[1, 0, 0]], float)
+    bcst = np.array([[1, 0, 0],[1, 0, 0]], float)
+
+    if kwargs != {}:
+        for key, value in kwargs.items():
+            if key == 'bcsu':
+                bcsu = value
+            elif key == 'bcsw':
+                bcsw = value
+            elif key == 'bcst':
+                bcst = value
+            else:
+                print("kwarg value not understood %s == %s" %(key, value))
+                print("ignored")
+
+
+    # setup indices for each field depending on BCs
+    iu0 = 0
+    iun = ncheb
+    if bcsu[0, 1] == 0:
+        # Dirichlet at z=-1/2
+        iu0 = 1
+    if bcsu[1, 1] == 0:
+        # Dirichlet ar z=1/2
+        iun = ncheb-1
+
+    iw0 = 0
+    iwn = ncheb
+    if bcsw[0, 1] == 0:
+        # Dirichlet at z=-1/2
+        iw0 = 1
+    if bcsw[1, 1] == 0:
+        # Dirichlet ar z=1/2
+        iwn = ncheb-1
+
+    it0 = 0
+    itn = ncheb
+    if bcst[0, 1] == 0:
+        # Dirichlet at z=-1/2
+        it0 = 1
+    if bcst[1, 1] == 0:
+        # Dirichlet ar z=1/2
+        itn = ncheb-1
+
+    # compute differentiation matrices
+    # For horizontal velocity
+    d2u, d1u, phipu, phimu = dm.cheb2bc(ncheb, bcsu)
+    # For vertical velocity
+    d2w, d1w, phipw, phimw = dm.cheb2bc(ncheb, bcsw)
+    # For temperature
+    d2t, d1t, phipw, phimw = dm.cheb2bc(ncheb, bcst)
+    # For pressure. No BCs but side values needed or removed
+    # depending on the BCs for W. number of lines need to be
+    # the same as that of d2w and depends on bcsw.
+    ddm = dm.chebdif(ncheb, 1)
+    d1p = 2.*ddm[0, iw0:iwn, :]
+    d1w = 2.*ddm[0, :, iw0:iwn]
+
+    # identity
+    ieye = np.eye(ncheb, ncheb)
+
+    # scale to [-0.5, 0.5]
+    d2u = 4.*d2u
+    d2w = 4.*d2w
+    d2t = 4.*d2t
+
+    # Prandtl number
+    pra = 1.
+    # square of the wave number
+    wn2 = wnk**2.
+    np.set_printoptions(precision=2, suppress=True, formatter={'float': '{: 0.2f}'.format})
+    # construction of the lhs matrix. Submatrices' dimensions must match
+    # first line: p
+    app = np.zeros((ncheb, ncheb))
+    apu = -1j*wnk*ieye[:, iu0:iun]
+    apw = -d1w
+    apt = np.zeros((ncheb, itn-it0))
+    # second line: u
+    aup = -1j*wnk*ranum*ieye[iu0:iun, :]
+    auu = pra*(d2u-wn2*ieye[iu0:iun, iu0:iun])
+    auw = np.zeros((iun-iu0, iwn-iw0))
+    aut = np.zeros((iun-iu0, itn-it0))
+    # third line: w
+    awp = -ranum*d1p
+    awu = np.zeros((iwn-iw0, iun-iu0))
+    aww = pra*(d2w-wn2*ieye[iw0:iwn, iw0:iwn])
+    awt = -ranum*ieye[iw0:iwn, it0:itn]
+    # fourth line: t
+    atp = np.zeros((itn-it0, ncheb))
+    atu = np.zeros((itn-it0, iun-iu0))
+    # Assume here the basic state to be linear of z
+    atw = -ieye[it0:itn, iw0:iwn]
+    att = d2t-wn2*ieye[it0:itn, it0:itn]
+    abig = np.concatenate((np.concatenate((app, apu, apw, apt), axis=1),
+                            np.concatenate((aup, auu, auw, aut), axis=1),
+                            np.concatenate((awp, awu, aww, awt), axis=1),
+                            np.concatenate((atp, atu, atw, att), axis=1),
+                            ))
+
+# construction of the rhs matrix: identity almost everywhere
+    nbig = abig.shape[0]
+    nsmall = nbig-app.shape[0]
+    bbig = np.eye(nbig)
+    # 0 for pressure
+    bbig[range(d1p.shape[1]), range(d1p.shape[1])] = 1.e-10
+    # Find the eigenvalues
+    egv, rvec = linalg.eig(abig, bbig, right=True)
+    leig = np.argmax(np.real(egv))
+    return egv[leig]
+
+
+def eigval_freeslip(wnk, ranum, ncheb, **kwargs):
     """eigenvalue for given wavenumber and Rayleigh number with Freeslip BCs"""
 
     # second order derivative.
@@ -55,7 +194,7 @@ def eigval_freeslip(wnk, ranum, ncheb):
     lmbda = -np.sort(-np.real(egv))
     return lmbda[0]
 
-def eigval_noslip(wnk, ranum, ncheb):
+def eigval_noslip(wnk, ranum, ncheb, **kwargs):
     """eigenvalue for given wavenumber and Rayleigh number for Noslip BCs"""
 
     # second order derivative.
@@ -84,12 +223,12 @@ def eigval_noslip(wnk, ranum, ncheb):
     lmbda = -np.sort(-np.real(egv))
     return lmbda[0]
 
-def search_ra(kwn, ray, ncheb, eigfun):
+def search_ra(kwn, ray, ncheb, eigfun, **kwargs):
     """find rayleigh number ray which gives neutral stability"""
     ray0 = ray/math.sqrt(1.2)
     ray1 = ray*math.sqrt(1.2)
-    la0 = eigfun(kwn, ray0, ncheb)
-    la1 = eigfun(kwn, ray1, ncheb)
+    la0 = np.real(eigfun(kwn, ray0, ncheb, **kwargs))
+    la1 = np.real(eigfun(kwn, ray1, ncheb, **kwargs))
     while la0 > 0. or la1 < 0.:
         if la0 > 0.:
             ray1 = ray0
@@ -97,11 +236,11 @@ def search_ra(kwn, ray, ncheb, eigfun):
         if la1 < 0.:
             ray0 = ray1
             ray1 = 2.*ray1
-        la0 = eigfun(kwn, ray0, ncheb)
-        la1 = eigfun(kwn, ray1, ncheb)
+        la0 = np.real(eigfun(kwn, ray0, ncheb, **kwargs))
+        la1 = np.real(eigfun(kwn, ray1, ncheb, **kwargs))
     while la1-la0 > 0.001:
         raym = (ray0+ray1)/2.
-        lam = eigfun(kwn, raym, ncheb)
+        lam = np.real(eigfun(kwn, raym, ncheb, **kwargs))
         if lam < 0.:
             la0 = lam
             ray0 = raym
@@ -110,12 +249,12 @@ def search_ra(kwn, ray, ncheb, eigfun):
             ray1 = raym
     return (ray0*la1-ray1*la0)/(la1-la0)
 
-def ra_ks(rag, wng, ncheb, eigfun):
+def ra_ks(rag, wng, ncheb, eigfun, **kwargs):
     """finds the minimum in the Ra-wn curve"""
     # find 3 values of Ra for 3 different wave numbers
     eps = [0.1, 0.01]
     wns = np.linspace(wng-eps[0], wng+2*eps[0], 3)
-    ray = [search_ra(kkx, rag, ncheb, eigfun) for kkx in wns]
+    ray = [search_ra(kkx, rag, ncheb, eigfun, **kwargs) for kkx in wns]
 
     # fit a degree 2 polynomial
     pol = np.polyfit(wns, ray, 2)
@@ -125,14 +264,14 @@ def ra_ks(rag, wng, ncheb, eigfun):
     for i, err in enumerate([0.03, 0.001]):
         while np.abs(kmin-wns[1]) > err:
             wns = np.linspace(kmin-eps[i], kmin+eps[i], 3)
-            ray = [search_ra(kkx, rag, ncheb, eigfun) for kkx in wns]
+            ray = [search_ra(kkx, rag, ncheb, eigfun, **kwargs) for kkx in wns]
             pol = np.polyfit(wns, ray, 2)
             kmin = -0.5*pol[1]/pol[0]
             rag = ray[1]
 
     return rag, kmin
 
-def findplot_rakx(ncheb, eigfun, title):
+def findplot_rakx(ncheb, eigfun, title, **kwargs):
     """
     Finds the minimum and plots Ra(kx)
 
@@ -142,12 +281,12 @@ def findplot_rakx(ncheb, eigfun, title):
     eigfun = name of the eigenvalue finding function
     title     = string variable to use in figure name
     """
-    ramin, kxmin = ra_ks(600, 2, NCHEB, eigfun)
-    print title+': Ra=', ramin, 'kx=', kxmin
+    ramin, kxmin = ra_ks(600, 2, ncheb, eigfun, **kwargs)
+    print(title+': Ra=', ramin, 'kx=', kxmin)
 
     # plot Ra as function of wavenumber
-    wnum = np.linspace(0.3, 8, 100)
-    rayl = [search_ra(kxwn, 600, NCHEB, eigfun) for kxwn in wnum]
+    wnum = np.linspace(0.4, 8, 100)
+    rayl = [search_ra(kxwn, ramin, ncheb, eigfun, **kwargs) for kxwn in wnum]
 
     fig = plt.figure()
     plt.plot(wnum, rayl, linewidth=2)
@@ -168,3 +307,8 @@ if COMPUTE_FREESLIP:
 if COMPUTE_NOSLIP:
     # find the minimum - Noslip
     findplot_rakx(NCHEB, eigval_noslip, 'Noslip')
+
+if COMPUTE_FREERIGID:
+    # using the more general function
+    findplot_rakx(NCHEB, eigval_general, 'FreeRigid', bcsu=np.array([[1, 0, 0],[0, 1, 0]], float))
+
