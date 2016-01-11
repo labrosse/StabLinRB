19888e695e15946ee1936a7aaa66eda14d279f30
Stéphane Labrosse <stephane.labrosse@ens-lyon.fr>
2016-01-11 11:21:32 +0100

    Plot of the profiles of U, W, T and P added

--- crit_ra.py
+++ crit_ra.py
@@ -12,13 +12,13 @@ from scipy import linalg
 import dmsuite.dmsuite as dm
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sprintf
 
 ######## Options #######
 # Free slip at both boudaries
 COMPUTE_FREESLIP = False
 # No slip at both boundaries
 COMPUTE_NOSLIP = False
+# Rigid bottom, free slip top
 COMPUTE_FREERIGID = True
 NCHEB = 10
 FTSZ = 14
@@ -42,14 +42,21 @@ def eigval_general(wnk, ranum, ncheb, **kwargs):
     wnk = horizontal wavenumber of the perturbation
     ranum = Rayleigh number
     ncheb = number of Chebyshev points in vertical direction
+    **kwargs :
+    bcsu=array of boundary conditions for u
+    bcsw=array of boundary conditions for w
+    bcst=array of boundary conditions for t
+    plot_eigvec=True|False (default) whether to return the (right) eigenvector
 
     OUTPUT
     eigv = eigenvalue with the largest real part (fastest growing if positive).
+    eigvec = corresponding eigen vector
     """
-    # Default boundary conditions. 
-    bcsu = np.array([[1, 0, 0],[1, 0, 0]], float)
-    bcsw = np.array([[1, 0, 0],[1, 0, 0]], float)
-    bcst = np.array([[1, 0, 0],[1, 0, 0]], float)
+    # Default boundary conditions : rigid-rigid
+    bcsu = np.array([[1, 0, 0], [1, 0, 0]], float)
+    bcsw = np.array([[1, 0, 0], [1, 0, 0]], float)
+    bcst = np.array([[1, 0, 0], [1, 0, 0]], float)
+    output_eigvec = False
 
     if kwargs != {}:
         for key, value in kwargs.items():
@@ -59,6 +66,8 @@ def eigval_general(wnk, ranum, ncheb, **kwargs):
                 bcsw = value
             elif key == 'bcst':
                 bcst = value
+            elif key == 'plot_eigvec':
+                output_eigvec = value
             else:
                 print("kwarg value not understood %s == %s" %(key, value))
                 print("ignored")
@@ -102,7 +111,10 @@ def eigval_general(wnk, ranum, ncheb, **kwargs):
     # For pressure. No BCs but side values needed or removed
     # depending on the BCs for W. number of lines need to be
     # the same as that of d2w and depends on bcsw.
-    ddm = dm.chebdif(ncheb, 1)
+    if output_eigvec:
+        xxt, ddm = dm.chebdif(ncheb, 1, outputx=True)
+    else:
+        ddm = dm.chebdif(ncheb, 1)
     d1p = 2.*ddm[0, iw0:iwn, :]
     d1w = 2.*ddm[0, :, iw0:iwn]
 
@@ -118,7 +130,6 @@ def eigval_general(wnk, ranum, ncheb, **kwargs):
     pra = 1.
     # square of the wave number
     wn2 = wnk**2.
-    np.set_printoptions(precision=2, suppress=True, formatter={'float': '{: 0.2f}'.format})
     # construction of the lhs matrix. Submatrices' dimensions must match
     # first line: p
     app = np.zeros((ncheb, ncheb))
@@ -142,10 +153,10 @@ def eigval_general(wnk, ranum, ncheb, **kwargs):
     atw = -ieye[it0:itn, iw0:iwn]
     att = d2t-wn2*ieye[it0:itn, it0:itn]
     abig = np.concatenate((np.concatenate((app, apu, apw, apt), axis=1),
-                            np.concatenate((aup, auu, auw, aut), axis=1),
-                            np.concatenate((awp, awu, aww, awt), axis=1),
-                            np.concatenate((atp, atu, atw, att), axis=1),
-                            ))
+                           np.concatenate((aup, auu, auw, aut), axis=1),
+                           np.concatenate((awp, awu, aww, awt), axis=1),
+                           np.concatenate((atp, atu, atw, att), axis=1),
+                          ))
 
 # construction of the rhs matrix: identity almost everywhere
     nbig = abig.shape[0]
@@ -154,9 +165,15 @@ def eigval_general(wnk, ranum, ncheb, **kwargs):
     # 0 for pressure
     bbig[range(d1p.shape[1]), range(d1p.shape[1])] = 1.e-10
     # Find the eigenvalues
-    egv, rvec = linalg.eig(abig, bbig, right=True)
+    if output_eigvec:
+        egv, rvec = linalg.eig(abig, bbig, right=True)
+    else:
+        egv = linalg.eig(abig, bbig, right=False)
     leig = np.argmax(np.real(egv))
-    return egv[leig]
+    if output_eigvec:
+        return egv[leig], rvec[:, leig], xxt
+    else:
+        return egv[leig]
 
 
 def eigval_freeslip(wnk, ranum, ncheb, **kwargs):
@@ -279,14 +296,35 @@ def findplot_rakx(ncheb, eigfun, title, **kwargs):
     ----------
     ncheb  = number of Chebyshev points in the calculation
     eigfun = name of the eigenvalue finding function
-    title     = string variable to use in figure name
+    title  = string variable to use in figure name
+    **kwargs: most are just to be passed on to eigfun
+    plot_eigvec (True|False) controls whether to plot the eigenvector
+    of the first ustable mode. Default is False
     """
-    ramin, kxmin = ra_ks(600, 2, ncheb, eigfun, **kwargs)
+    bcsu = np.array([[1, 0, 0], [1, 0, 0]], float)
+    bcsw = np.array([[1, 0, 0], [1, 0, 0]], float)
+    bcst = np.array([[1, 0, 0], [1, 0, 0]], float)
+    plot_eigvec = False
+    if kwargs != {}:
+        for key, value in kwargs.items():
+            if key == 'plot_eigvec':
+                plot_eigvec = value
+                # alternate kwargs to not output eigenvector
+                kwargs2 = kwargs.copy()
+                kwargs2['plot_eigvec'] = False
+            elif key == 'bcsu':
+                bcsu = value
+            elif key == 'bcsw':
+                bcsw = value
+            elif key == 'bcst':
+                bcst = value
+
+    ramin, kxmin = ra_ks(600, 2, ncheb, eigfun, **kwargs2)
     print(title+': Ra=', ramin, 'kx=', kxmin)
 
     # plot Ra as function of wavenumber
-    wnum = np.linspace(0.4, 8, 100)
-    rayl = [search_ra(kxwn, ramin, ncheb, eigfun, **kwargs) for kxwn in wnum]
+    wnum = np.linspace(0.4, 8, 10)
+    rayl = [search_ra(kxwn, ramin, ncheb, eigfun, **kwargs2) for kxwn in wnum]
 
     fig = plt.figure()
     plt.plot(wnum, rayl, linewidth=2)
@@ -299,6 +337,99 @@ def findplot_rakx(ncheb, eigfun, title, **kwargs):
     plt.savefig('Ra_kx_'+title+'.pdf', format='PDF')
     plt.close(fig)
 
+    if plot_eigvec:
+        egv, eigvec, zzr = eigfun(kxmin, ramin, ncheb, **kwargs)
+        # setup indices for each field depending on BCs
+        iu0 = ncheb
+        iun = 2*ncheb
+        iuz0 = 0
+        iuzn = ncheb
+        if bcsu[0, 1] == 0:
+            # Dirichlet at z=-1/2
+            iun -= 1
+#            iuz0 += 1
+        if bcsu[1, 1] == 0:
+            # Dirichlet ar z=1/2
+            iun = iun-1
+#            iuzn += 1
+
+        iw0 = iun
+        iwn = iun+ncheb
+        iwz0 = 0
+        iwzn = ncheb
+        if bcsw[0, 1] == 0:
+            # Dirichlet at z=-1/2
+            iwn = iwn-1
+#            iwz0 += 1
+        if bcsw[1, 1] == 0:
+            # Dirichlet ar z=1/2
+            iwn = iwn-1
+#            iwzn -= 1
+
+        it0 = iwn
+        itn = iwn+ncheb
+        itz0 = 0
+        itzn = ncheb
+        if bcst[0, 1] == 0:
+            # Dirichlet at z=-1/2
+            itn = itn-1
+#            itz0 += 1
+        if bcst[1, 1] == 0:
+            # Dirichlet ar z=1/2
+            itn = itn-1
+#            itzn -= 1
+
+        # now split the eigenvector into the different fields
+        pmod = eigvec[0:ncheb]/np.max(np.abs(eigvec[0:ncheb]))
+        umod = eigvec[iu0:iun]/np.max(np.abs(eigvec[iu0:iun]))
+        # add boundary values in Dirichlet case
+        if bcsu[0, 1] == 0:
+            # Dirichlet at z=-1/2
+            umod = np.insert(umod, 0, bcsu[0, 2])
+        if bcsu[1, 1] == 0:
+            # Dirichlet ar z=1/2
+            umod = np.append(umod, bcsu[1, 2])
+        wmod = eigvec[iw0:iwn]/np.max(np.abs(eigvec[iw0:iwn]))
+        if bcsw[0, 1] == 0:
+            # Dirichlet at z=-1/2
+            wmod = np.insert(wmod, [0], [bcsw[0, 2]])
+        if bcsw[1, 1] == 0:
+            # Dirichlet ar z=1/2
+            wmod = np.append(wmod, bcsw[1, 2])
+        tmod = eigvec[it0:itn]/np.max(np.abs(eigvec[it0:itn]))
+        if bcst[0, 1] == 0:
+            # Dirichlet at z=-1/2
+            tmod = np.insert(tmod, 0, bcst[0, 2])
+        if bcst[1, 1] == 0:
+            # Dirichlet ar z=1/2
+            tmod = np.append(tmod, bcst[1, 2])
+
+        # define the z values on which to interpolate modes
+        zpl = np.linspace(-1, 1, 100)
+        # interpolate
+        upl = dm.chebint(umod, zpl)
+        wpl = dm.chebint(wmod, zpl)
+        tpl = dm.chebint(tmod, zpl)
+        ppl = dm.chebint(pmod, zpl)
+        # plot the norm of the mode profiles
+        fig, axe = plt.subplots(1, 4, sharey=True)
+        plt.setp(axe, xlim=[-0.1, 1.1], ylim=[-0.5, 0.5], xticks=[0.1, 0.5, 0.9])
+        axe[0].plot(np.abs(upl), zpl/2)
+        axe[0].plot(np.abs(umod), zzr/2, "o", label=r'$U$')
+        axe[0].set_ylabel(r'$z$', fontsize=FTSZ)
+        axe[0].set_xlabel(r'$|U|$', fontsize=FTSZ)
+        axe[1].plot(np.abs(wpl), zpl/2)
+        axe[1].plot(np.abs(wmod), zzr/2, "o", label=r'$W$')
+        axe[1].set_xlabel(r'$|W|$', fontsize=FTSZ)
+        axe[2].plot(np.abs(tpl), zpl/2)
+        axe[2].plot(np.abs(tmod), zzr/2, "o", label=r'$\theta$')
+        axe[2].set_xlabel(r'$|\theta|$', fontsize=FTSZ)
+        axe[3].plot(np.abs(ppl), zpl/2)
+        axe[3].plot(np.abs(pmod), zzr/2, "o", label=r'$P$')
+        axe[3].set_xlabel(r'$|P|$', fontsize=FTSZ)
+        plt.savefig("Mode_profiles"+title+".pdf", format='PDF')
+
+
 
 if COMPUTE_FREESLIP:
     # find the minimum - Freeslip
@@ -310,5 +441,7 @@ if COMPUTE_NOSLIP:
 
 if COMPUTE_FREERIGID:
     # using the more general function
-    findplot_rakx(NCHEB, eigval_general, 'FreeRigid', bcsu=np.array([[1, 0, 0],[0, 1, 0]], float))
+    findplot_rakx(NCHEB, eigval_general, 'RigidFree',
+                  bcsu=np.array([[1, 0, 0], [0, 1, 0]],
+                                float), plot_eigvec=True)
 
